8ba9370a161baf4ce6d2a0bf6c849a4f759ea120
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-12-21 22:29:54 -0800

    adding license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![License](https://img.shields.io/static/v1?label=license&message=MIT&color=ff69b4)`;
+} else {
+    return "";
+}
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+ if (license) {
+        return `https://choosealicense.com/licenses/${license}`;
+    } else {
+        return "#";
+    }
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
